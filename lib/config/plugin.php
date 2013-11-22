f8536879b83c3c585441206bd3e947c850d800dd
klxqz <klxqz@mail.ru>
2013-11-22 14:25:50 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,17 +3,16 @@
 return array(
     'name' => '3D Облако тегов',
     'description' => 'Вращающийся 3D шар из тегов',
-    'vendor'=>903438,
-    'version'=>'1.0.0',
-    'img'=>'img/tags3d.png',
+    'vendor' => '985310',
+    'version' => '1.0.1',
+    'img' => 'img/tags3d.png',
     'shop_settings' => true,
-    'frontend'    => true,
-    'icons'=>array(
-        16=>'img/tags3d.png',
+    'frontend' => true,
+    'icons' => array(
+        16 => 'img/tags3d.png',
     ),
     'handlers' => array(
         'frontend_nav' => 'frontendNav'
     ),
-
 );
 //EOF
